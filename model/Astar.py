7d96708933cd8ef1fe782a90d8e1af01c366d41b
yelj <yeyeleijun@gmail.com>
2023-06-06 09:37:33 +0800

    add heuristic function

--- model/Astar.py
+++ model/Astar.py
@@ -51,11 +51,23 @@ class AStar:
         self.pq.put((0, self.start))
         self.free_grid = np.ones(self.grid_size, dtype=np.uint8)  # 1 is valid
 
+    def get_directions(self):
+        directions = []
+        for k in range(self.dim):
+            direction = [0] * self.dim
+            direction[k] = 1
+            directions.append(tuple(direction))
+            direction = [0] * self.dim
+            direction[k] = -1
+            directions.append(tuple(direction))
+        return directions
+
     def explore_obstacle(self, obstacle_coords, tolerance=0):
         """
         :param obstacle_coords: list of all obstacles organized as tuples.
         :param tolerance: the space should be extended outward by a certain distance, default is 0.
         """
+        self.obstacle_coords = obstacle_coords
         for i in range(len(obstacle_coords)):
             coord0, coord1 = obstacle_coords[i]
             coord0 = tuple(map(lambda item: int(item) - tolerance, coord0))
@@ -63,14 +75,29 @@ class AStar:
             for coord in product(*(range(s, e) for s, e in zip(coord0, coord1))):
                 if self.coord_valid(coord, self.space_coords[0], self.space_coords[1]):
                     self.free_grid[coord] = 0
+
         return None
 
-    def set_energy(self, distance=2):
-        pass
+    def set_energy(self, lb=1, ub=25, step=2):
+        thre = (ub - lb) // step
+        temp = tuple(map(lambda x: x - thre * 2, self.grid_size))
+        energy = np.ones(temp, dtype=np.float32) * ub
+        for k in range(lb, step, ub+step):
+            assert k > 0
+            energy = np.pad(energy, pad_width=1, mode='constant', constant_values=k)
+
+        # update energy
+        for i in range(len(self.obstacle_coords)):
+            coord0, coord1 = self.obstacle_coords[i]
+            obstacle_size = tuple(coord0[i] - coord1[i] for i in range(len(coord0[0])))
+            obstacle = np.ones(obstacle_size, dtype=np.float32) * float('inf')
+            obstacle = np.pad(obstacle, pad_width=1, mode='constant', constant_values=5)
+
+        return energy
 
     def base_cost(self, p, w_path=1, w_bend=1, w_energy=1):
         # TODO: add a energy function.
-        f = w_path * p.depth + w_bend * p.n_cp + w_energy * 0
+        f = w_path * p.depth + w_bend * p.n_cp + w_energy * self.energy[p.coord]
         return f
 
     def heuristic_cost(self, p_coord, end):
@@ -128,14 +155,8 @@ class AStar:
         start_time = time.time()
 
         # Process all neighbors
-        directions = []
-        for k in range(self.dim):
-            direction = [0] * self.dim
-            direction[k] = 1
-            directions.append(tuple(direction))
-            direction = [0] * self.dim
-            direction[k] = -1
-            directions.append(tuple(direction))
+        directions = self.get_directions()
+        self.energy = self.set_energy()
 
         while not self.pq.empty():
             # find the node with minimum cost
